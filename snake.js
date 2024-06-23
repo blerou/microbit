9840c76ca08a90d80a605cb7bb50ff77b89aa621
blerou <sulik.szabolcs@gmail.com>
2024-06-23 13:41:03 +0200

    snake: one direction change per move

--- snake.js
+++ snake.js
@@ -1,18 +1,29 @@
 input.onPinPressed(TouchPin.P0, function () {
     gameOn = !(gameOn)
 })
-input.onPinPressed(TouchPin.P1, function () {
-    speed = (speed + 1) % speedSteps
-})
+function showSnake () {
+    for (let p of snake) {
+        led.plot(p[0], p[1])
+    }
+}
 input.onButtonPressed(Button.A, function () {
-    if (dx != 0) {
-        dy = dx * -1
-        dx = 0
-    } else if (dy != 0) {
-        dx = dy
-        dy = 0
+    if (!(changeDirection)) {
+        if (dx != 0) {
+            dy = dx * -1
+            dx = 0
+        } else if (dy != 0) {
+            dx = dy
+            dy = 0
+        }
+        changeDirection = true
     }
 })
+function nextHead () {
+    head = snake[0]
+    x = ((head[0] + dx) % 5 + 5) % 5
+    y = ((head[1] + dy) % 5 + 5) % 5
+    return [x, y]
+}
 input.onButtonPressed(Button.B, function () {
     if (dx != 0) {
         dy = dx
@@ -22,39 +33,35 @@ input.onButtonPressed(Button.B, function () {
         dy = 0
     }
 })
-function showSnake() {
-    for (let p of snake) {
-        led.plot(p[0], p[1])
-    }
-}
-let tick = 0
+input.onPinPressed(TouchPin.P1, function () {
+    speed = (speed + 1) % speedSteps
+})
 let dy = 0
+let changeDirection = false
 let speed = 0
 let speedSteps = 0
 let dx = 0
 let snake: number[][] = []
 let gameOn = false
-let head, x, y, nextHead, tail, sleep
+let head, x, y, tail, sleep, nh
 // init
 gameOn = true
 snake = [[2, 3], [2, 4]]
 dx = 1
 speedSteps = 3
-speed = speedSteps
+speed = speedSteps-1
 // game loop
 basic.forever(function () {
     sleep = speed * 200 + 100
     showSnake()
     basic.pause(sleep)
-    tick += 1
-    if (gameOn && tick % 2 == 0) {
+    if (gameOn) {
         // move head
-        head = snake[0]
-        x = ((head[0] + dx) % 5 + 5) % 5
-        y = ((head[1] + dy) % 5 + 5) % 5
-        nextHead = [x, y]
-        snake.unshift(nextHead)
+        nh = nextHead()
+        snake.unshift(nh)
         tail = snake.pop()
         led.unplot(tail[0], tail[1])
+        // reinit state
+        changeDirection = false
     }
 })
